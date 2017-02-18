383208c7c061cfa2073431b5858848d70a37dbe9
Tom Bassett <tbassett44@gmail.com>
2017-02-18 12:42:30 -0700

    working async and read photos and videos

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ exports.printMsg = function() {
 exports.process = function(opts){
 	var unzip=require('unzip');
 	var app={
+		out:{},
 		init:function(opts){
 			var self=this;
 			self.fs=require('fs');
 			self.cheerio=require('cheerio');
 			self.md5File = require('md5-file');
+			self.async = require('async');
 			self.opts=opts;
 			self.unzip(function(){
 				self.processData();
@@ -55,44 +57,109 @@ exports.process = function(opts){
 
 		    return urlArray;
 		},
+		getDirectoryContents:function(path,cb){
+			var self=this;
+			var items=self.fs.readdirSync(path);
+			var out={};	
+			var returnobj=[]	 
+		    for (var i=0; i<items.length; i++) {
+		    	if(items[i]=='index.htm') continue;
+		    	var tpath=path+'/'+items[i];
+		    	if(self.fs.lstatSync(tpath).isDirectory()){
+		    		out[items[i]]=self.getDirectoryContents(tpath);
+		    	}else{
+		    		returnobj.push(tpath);
+		    	}
+		    }
+		    //console.log(out)
+		    if(cb) cb(out);
+		    else return returnobj;
+		},
+		getDirectoryContents2:function(path,cb){
+			var self=this;
+			var items=self.fs.readdirSync(path);
+			var out={};	
+		    for (var i=0; i<items.length; i++) {
+		    	if(items[i]=='index.htm') continue;
+		    	var tpath=path+'/'+items[i];
+		    	var p=items[i].split('.');
+		    	if(!out[p[0]]) out[p[0]]={};
+		    	if(p[1]=='mp4'){
+		    		out[p[0]]['video']=tpath;
+		    	}else{
+		    		out[p[0]]['cover']=tpath;
+		    	}
+		    }
+		    //console.log(out)
+		    cb(out);
+		},
 		processData:function(){
 			var self=this;
-			self.fs.readFile(self.saveto+'/html/timeline.htm', 'utf8', function(err, data){
-			 if (err){
-			     return console.log(err);
-			 }
-			 var $ = self.cheerio.load(data);
-			 $('.comment').each(function(i,v){
-			 	var text=$(v).text();
-			 	var urls=self.findUrls(text);
-			 	if(urls.length){
-			 		for (var i = 0; i < urls.length; i++) {
-			 			var v=urls[i];
-			 			self.opts.onLink(v);
-			 		};
-			 	}
-			 })
-			});
-			self.fs.readFile(self.saveto+'/html/photos.htm', 'utf8', function(err, data){
-			 if (err){
-			     return console.log(err);
-			 }
-			 var $ = self.cheerio.load(data);
-			 $('.block').each(function(i,v){
-			 	var img=$(v).find('img').attr('src').replace('../',self.saveto+'/');
-			 	self.opts.onImage(img);
-			 })
-			});
-			self.fs.readFile(self.saveto+'/html/videos.htm', 'utf8', function(err, data){
-			 if (err){
-			     return console.log(err);
-			 }
-			 var $ = self.cheerio.load(data);
-			 $('.block').each(function(i,v){
-			 	var vid=$(v).find('a').attr('href').replace('../',self.saveto+'/');
-			 	self.opts.onVideo(vid);
-			 })
-			});
+			var queue = self.async.queue(function (opts, fin) {
+			    switch(opts.type){
+			    	case 'links':
+			    		self.fs.readFile(self.saveto+'/html/timeline.htm', 'utf8', function(err, data){
+						 if (err){
+						     return console.log(err);
+						 }
+						 var $ = self.cheerio.load(data);
+						 app.out.links=[];
+						 $('.comment').each(function(i,v){
+						 	var text=$(v).text();
+						 	var urls=self.findUrls(text);
+						 	if(urls.length){
+						 		for (var i = 0; i < urls.length; i++) {
+						 			var v=urls[i];
+						 			if(self.opts.onLink) self.opts.onLink(v);
+						 			app.out.links.push(v);
+						 		};
+						 	}
+						 })
+						 fin();
+						});
+			    	break;
+			    	case 'photos':
+			    		self.getDirectoryContents(self.saveto+'/photos',function(data){
+							if(self.opts.onImage){
+								for(album in data){
+									var talbum=data[album];
+									for (var i = 0; i < talbum.length; i++) {
+										var img=talbum[i];
+										self.opts.onImage(img,album);
+									};
+								}
+							}
+							app.out.images=data;
+							fin();
+						});
+			    	break;
+			    	case 'videos':
+			    		self.getDirectoryContents2(self.saveto+'/videos',function(data){
+							if(self.opts.onVideo){
+								for (video in data) {
+									var tvideo=data[video];
+									self.opts.onVideo(tvideo,video);
+								};
+							}
+							app.out.videos=data;
+							fin();
+						});
+			    	break;
+			    }
+			}, 100);
+			queue.drain = function() {
+				if(app.opts.onComplete) app.opts.onComplete(app.out)
+		        process.exit(0);
+			}
+			queue.push({
+				type:'links'
+			})
+			queue.push({
+				type:'photos'
+			})
+			queue.push({
+				type:'videos'
+			})
 		}
 	}
 	app.init(opts);
